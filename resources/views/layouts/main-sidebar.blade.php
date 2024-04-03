52353c3e282aeadc4f0bb710cc1019ed6ba18ed2
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-04-03 21:29:53 +0200

    add Whychooseus in sidebar

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -26,6 +26,18 @@
 						</ul>
 
                     </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()"  aria-expanded="false">
+							<i class="flaticon-381-networking"></i>
+							<span class="nav-text">Whychooseus</span>
+						</a>
+                        <ul aria-expanded="false">
+							<li><a href="{{ url('/Whychooseus') }}">Whychooseus</a></li>
+						</ul>
+                        <ul aria-expanded="false">
+							<li><a href="{{ url('/Whychooseus/create') }}">create</a></li>
+						</ul>
+
+                    </li>
                     
                     <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
 							<i class="flaticon-381-networking"></i>
